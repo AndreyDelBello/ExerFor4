bc1cafa2c56798ffa493d4209f5b1533240ac639
Andrey_Del_Bello <andrey.delbello2002@gmail.com>
2021-08-26 17:13:05 +0000

    ExerFor4

---
+++ ExerFor4.java
@@ -0,0 +1,10 @@
+public class ExerFor4 {
+    public static void main(String[]args){
+        int a=1;
+        for(int n=1;n<=10;n++){
+            a*=n;
+        }
+        System.out.println(a);
+    }
+    
+}
